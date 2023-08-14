36cf34c611a972faf1325ac215f9f5803519a19a
Jacint Blanch <jacintblanch5@gmail.com>
2023-08-14 17:21:00 +0200

    Create film context

---
+++ src/store/FilmContextProvider.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren, useCallback, useMemo, useState } from "react";
+import useFilmsApi from "../hooks/useFilmsApi";
+import FilmContext, { FilmContextStructure } from "./FilmsContext";
+import { Film } from "./types";
+
+const FilmContextProvider = ({
+  children,
+}: PropsWithChildren): React.ReactElement => {
+  const [films, setFilmsList] = useState<Film[]>([]);
+
+  const { getFilms } = useFilmsApi();
+
+  const loadFilms = useCallback(async () => {
+    const apiFilms = await getFilms();
+
+    const { results } = apiFilms;
+
+    setFilmsList([...results]);
+  }, [getFilms]);
+
+  const filmsContextValue = useMemo(
+    (): FilmContextStructure => ({
+      films,
+      loadFilms,
+    }),
+    [films, loadFilms],
+  );
+  return (
+    <FilmContext.Provider value={filmsContextValue}>
+      {children}
+    </FilmContext.Provider>
+  );
+};
+
+export default FilmContextProvider;
