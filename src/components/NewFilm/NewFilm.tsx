9333b5a8f0b19ca6ff669460b6f9cc3ef4dee5e1
Jacint Blanch <jacintblanch5@gmail.com>
2023-08-15 19:02:01 +0200

    Logic for adding new films

--- src/components/NewFilm/NewFilm.tsx
+++ src/components/NewFilm/NewFilm.tsx
@@ -5,23 +5,46 @@ import "./NewFilm.css";
 const NewFilm = (): React.ReactElement => {
   const [canSubmit] = useState(false);
 
+  const initialValue = {
+    title: "",
+    poster: "",
+    director: "",
+    year: 0,
+  };
+
+  const [newFilm, setNewFilm] = useState(initialValue);
+
+  const { title, poster, director, year } = newFilm;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewFilm((newFilm) => ({
+      ...newFilm,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
   return (
     <form className="form-film">
       <div className="form-control">
         <label htmlFor="title">Título: </label>
-        <input type="text" id="title" />
+        <input type="text" onChange={handleChange} value={title} id="title" />
       </div>
       <div className="form-control">
         <label htmlFor="director">Dirección: </label>
-        <input type="text" id="director" />
+        <input
+          type="text"
+          onChange={handleChange}
+          value={director}
+          id="director"
+        />
       </div>
       <div className="form-control">
         <label htmlFor="year">Año: </label>
-        <input type="number" id="year" />
+        <input type="number" onChange={handleChange} value={year} id="year" />
       </div>
       <div className="form-control">
         <label htmlFor="poster">URL cartel: </label>
-        <input type="url" id="poster" />
+        <input type="url" onChange={handleChange} value={poster} id="poster" />
       </div>
       <div className="form-control">
         <Button disabled={!canSubmit}>Crear película</Button>
